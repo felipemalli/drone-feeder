9ed8ff2ef952b570959f717b49797f11da071c6d
Natã Elienai <nata.elienai@gmail.com>
2022-10-17 19:55:27 -0300

    feat: create Drone entity

---
+++ src/main/java/io/github/nataelienai/dronefeeder/drone/Drone.java
@@ -0,0 +1,52 @@
+package io.github.nataelienai.dronefeeder.drone;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import io.github.nataelienai.dronefeeder.delivery.Delivery;
+
+@Entity
+@Table(name = "drone")
+public class Drone {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "latitude", nullable = false)
+  private String latitude;
+
+  @Column(name = "longitude", nullable = false)
+  private String longitude;
+
+  @OneToMany(mappedBy = "drone", fetch = FetchType.LAZY)
+  private List<Delivery> deliveries;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+}
