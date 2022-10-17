31b139694b4a47d75f611d58dc8e124504b90376
Natã Elienai <nata.elienai@gmail.com>
2022-10-17 19:49:29 -0300

    feat: initial commit

---
+++ src/test/java/io/github/nataelienai/dronefeeder/DroneFeederApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.nataelienai.dronefeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneFeederApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
