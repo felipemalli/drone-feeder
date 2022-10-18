122db2cb94ae17e85fad299b03b6dbad476eb904
felipemalli <felipevahia@gmail.com>
2022-10-18 00:14:54 -0300

    feat: implement delivery controller

---
+++ src/main/java/io/github/nataelienai/dronefeeder/delivery/DeliveryController.java
@@ -0,0 +1,43 @@
+package io.github.nataelienai.dronefeeder.delivery;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class DeliveryController {
+  
+  @Autowired
+  private DeliveryService deliveryService;
+
+  @PostMapping("/")
+  public Delivery create(@RequestBody Delivery delivery) {
+    return deliveryService.create(delivery);
+  }
+
+  @GetMapping("/")
+  public List<Delivery> findAll() {
+    return deliveryService.findAll();
+  }
+
+  @PutMapping("/{id}")
+  public Delivery updateStatus(@PathVariable Long id, @RequestBody Status status) {
+    return deliveryService.updateStatus(id, status);
+  }
+
+  @PutMapping("/{id}/{droneId}")
+  public Delivery updateDrone(@PathVariable Long id, @PathVariable Long droneId) {
+    return deliveryService.updateDrone(id, droneId);
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    deliveryService.delete(id);
+  }
+
+}
