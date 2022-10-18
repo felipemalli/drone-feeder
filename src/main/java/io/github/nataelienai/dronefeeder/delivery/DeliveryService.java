11b83c990167182ce09b066a16620336d0e5f4f7
felipemalli <felipevahia@gmail.com>
2022-10-17 23:57:38 -0300

    feat: implement findAll  method for deliveryService

--- src/main/java/io/github/nataelienai/dronefeeder/delivery/DeliveryService.java
+++ src/main/java/io/github/nataelienai/dronefeeder/delivery/DeliveryService.java
@@ -1,5 +1,7 @@
 package io.github.nataelienai.dronefeeder.delivery;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,10 @@ public class DeliveryService {
   
   public Delivery create(Delivery delivery) {
     return deliveryRepository.save(delivery);
+  }
+
+  public List<Delivery> findAll() {
+    return deliveryRepository.findAll();
   }
 
 }
