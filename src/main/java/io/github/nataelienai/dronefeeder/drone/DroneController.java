7761117cec9f0295daa04ca81610289a5ba7000e
Natã Elienai <nata.elienai@gmail.com>
2022-10-18 11:59:14 -0300

    feat: add findById method to DroneController

--- src/main/java/io/github/nataelienai/dronefeeder/drone/DroneController.java
+++ src/main/java/io/github/nataelienai/dronefeeder/drone/DroneController.java
@@ -3,6 +3,7 @@ package io.github.nataelienai.dronefeeder.drone;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,11 @@ public class DroneController {
   @GetMapping
   public List<Drone> findAll() {
     return droneService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Drone findById(@PathVariable("id") Long id) {
+    return droneService.findById(id);
   }
 
 }
