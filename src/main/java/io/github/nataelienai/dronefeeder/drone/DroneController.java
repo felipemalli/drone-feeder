91be685d5df2477e70e0a6635b8b43bd410481b7
Natã Elienai <nata.elienai@gmail.com>
2022-10-18 17:31:19 -0300

    fix: add response status to endpoints

--- src/main/java/io/github/nataelienai/dronefeeder/drone/DroneController.java
+++ src/main/java/io/github/nataelienai/dronefeeder/drone/DroneController.java
@@ -2,6 +2,7 @@ package io.github.nataelienai.dronefeeder.drone;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,26 +28,31 @@ public class DroneController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Drone create(@RequestBody Drone drone) {
     return droneService.create(drone);
   }
 
   @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public List<Drone> findAll() {
     return droneService.findAll();
   }
 
   @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
   public Drone findById(@PathVariable Long id) {
     return droneService.findById(id);
   }
 
   @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
   public Drone update(@PathVariable Long id, @RequestBody Drone drone) {
     return droneService.update(id, drone);
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable Long id) {
     droneService.delete(id);
   }
